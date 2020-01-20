2c7baf808c7c86aac05c40269a6be2bebf42ae53
milomacphail <milomacphail@gmail.com>
2020-01-20 16:13:13 -0500

    Added array of chars to represent symbols.

---
+++ Arena.cs
@@ -0,0 +1,29 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatchingGame
+{
+    public partial class Arena : Form
+    {
+        Random random = new Random();
+
+        List<string> Icons = new List<string>()
+        {
+            "!", "!", "U", "U", "C", "C", "A", "A",
+            "b", "b", "k", "k", "M", "M", "1", "1"
+        };
+
+        public Arena()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
